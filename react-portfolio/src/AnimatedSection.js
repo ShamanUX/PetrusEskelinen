58fb9ec1303093d24bc8a2b3a78782a1017a541d
ShamanUX <petu.esk@gmail.com>
2023-06-21 00:01:02 +0300

    useInView is working correctly - detects when an element is visible and can trigger animation based on it

--- react-portfolio/src/AnimatedSection.js
+++ react-portfolio/src/AnimatedSection.js
@@ -1,7 +1,7 @@
 import './AnimatedSection.css'
 import {useSpring, animated} from 'react-spring'
-import {useEffect, useRef} from 'react';
-import { config } from 'react-transition-group';
+import {useEffect, useRef, useState} from 'react';
+import { InView, defaultFallbackInView, useInView } from 'react-intersection-observer';
 
 
 export default function AnimatedSection() 
@@ -11,25 +11,25 @@ export default function AnimatedSection()
         from: {opacity: 0}
     }))
     
-    const sectionRef = useRef(null);
-
-    useEffect(() => {
-        
-    })
-
-    const handleScroll = () => {
-
-        console.log("scroll happened");
-        
-        api.start({
-            from: {opacity: 0},
-            to: {opacity: 1},
-            config: {duration: 1000}
-        })
+    
+ 
+    const handleVisibilityChange = (inView) => {
+        if (inView) {
+            api.start({
+                from: {opacity: 0},
+                to: {opacity: 1},
+                config: {duration: 1000}
+            })
+        } 
     }
+    
+    const { ref: sectionRef, inView: sectionIsVisible } = useInView({
+        threshold: 0,
+        onChange: handleVisibilityChange,
+      });
 
     return(
-        <div className='section-container' onScroll={handleScroll()}>
+        <div className='section-container' ref={sectionRef} >
             <animated.h1 style={{...springs}}>title that should animate</animated.h1>
         </div>
     )
--- react-portfolio/src/Page.js
+++ react-portfolio/src/Page.js
@@ -4,30 +4,32 @@ import Burger from './Burger';
 import Carousel from './Carousel';
 import {useRef} from "react";
 import {useSpring, animated } from 'react-spring';
+import { useInView } from 'react-intersection-observer';
 
 
 
 function Page(props) {
     const handleBurgerClick = () => {
         api.start({
-            from: {transfrom: "translateX(0px)"},
-            to: {transfrom: "translateX(400px)"}
+            from: {},
+            to: {}
         })
     }
     const [ springs, api] = useSpring(() => ({  
-        from: {transfrom: "translateX(0px)"}
-    }))
+        from: {}
+    }));
+    
+
 
     return (
         // toplevel-container, a row-flex, allows the burger to work as intended 
         <div className='toplevel-container'>
+            
             <Burger clickFunction={handleBurgerClick}/>
             <animated.div className='page-container' style={{...springs}}>
-                
                 <div>
                  {props.pagebody}
                 </div>
-                    
             </animated.div>
         </div>
         
