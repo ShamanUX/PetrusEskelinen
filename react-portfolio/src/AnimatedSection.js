323449204cbfe9d59bdd4004c16e76770be758c1
ShamanUX <petu.esk@gmail.com>
2023-07-19 11:51:26 +0300

    gh-pages setup, videos

--- react-portfolio/src/AnimatedSection.js
+++ react-portfolio/src/AnimatedSection.js
@@ -14,16 +14,18 @@ export default function AnimatedSection(props)
         from: {x:0, opacity: 0}
     }))
     
+    
+
     const handleVisibilityChange = (inView) => {
         if (inView) {
             api.start(
                 {
-                    from: {x: -50, opacity: 0},
+                    from: {x: -20, opacity: 0},
                     to: {x: 0, opacity: 1},
                     delay: props.delay,
                     config: {
                         duration: 500,
-                        easing: easings.easeInCubic
+                        easing: easings.easeInOutQuart
                     }
                 }
             )
@@ -31,7 +33,7 @@ export default function AnimatedSection(props)
     }
     
     const { ref: sectionRef, inView } = useInView({
-        threshold: 0.25,
+        threshold: 1,
         onChange: handleVisibilityChange,
         triggerOnce: true,
       });
@@ -40,6 +42,14 @@ export default function AnimatedSection(props)
         <animated.div className='section-container'  style={{...springs}}  ref={sectionRef} >
             <animated.h1>{props.title}</animated.h1>
             <img src={props.image} style={{width: "80%"}}></img>
+            {/* If a video prop exists, display the video element. */}
+            {props.video ? 
+                <video autoPlay loop muted  style={{width: "80%", borderRadius: "20px"}}>
+                    <source src={props.video} type="video/mp4" />
+                </video> 
+                :
+                null
+            }
         </animated.div>
     )
 }
--- react-portfolio/src/Home.js
+++ react-portfolio/src/Home.js
@@ -7,6 +7,9 @@ import App from './SlickSlider.js'
 
 import AnimatedSection from "./AnimatedSection";
 import rajatonCollage from "./images/Rajatontaidecollage.png"
+import dashboardCollage from "./images/Dashboardcollage.png"
+import barrelrollDemo from "./videos/Barrelroll\ demo.mp4"
+import soundscapeDemo from "./videos/Soundscape\ demo.mp4"
 import csharp from './images/c-sharp logo.png'
 import webdev from './images/html5-logo-31816.png'
 import python from './images/Python-Logo.png'
@@ -16,7 +19,7 @@ import node from './images/nodejs-1-logo.png'
 import figma from './images/Figma-1-logo.png'
 import reactlogo from  './images/reactlogo.svg';
 
-const OPTIONS = { loop: true }
+const OPTIONS = { loop: true, dragFree: true,}
 const AUTOPLAYOPTIONS = {delay: 2000}
 const SLIDE_COUNT = 8
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
@@ -26,21 +29,11 @@ export default function Home() {
             <div>
                 <header>
                     <div className='header-content'>
-                        <h1> User-friendly software development </h1>
-                        
-                        <div className="logoList">
-                            <AnimatedBlock delay={200} source={reactlogo}/>
-                            <AnimatedBlock delay={250} source={webdev}/>
-                            <AnimatedBlock delay={300} source={figma}/>
-                            <AnimatedBlock delay={350} source={unity}/>
-                            <AnimatedBlock delay={400} source={node}/>
-                            <AnimatedBlock delay={450} source={sql}/>
-                            <AnimatedBlock delay={500} source={csharp}/>
-                            <AnimatedBlock delay={550} source={python}/>
-                        </div>                     
+                        <h1> User-friendly software development & design </h1>
+                        <EmblaCarousel options={OPTIONS} slides={SLIDES} autoplayOptions={AUTOPLAYOPTIONS}/>
                     </div>
                 </header>
-                <EmblaCarousel options={OPTIONS} slides={SLIDES} autoplayOptions={AUTOPLAYOPTIONS}/>
+                
                 <div className="section-grid">
                     <AnimatedSection 
                         title="Webpage development & design for Rajaton Taide -festival"
@@ -49,16 +42,18 @@ export default function Home() {
                     />
                     <AnimatedSection 
                         title="UI dashboard design for a resource management solution" 
-                        delay={500}
+                        delay={700}
+                        image={dashboardCollage}
                     />
                     <AnimatedSection 
                         title="BarrelRoll: 2D space shooter"
                         delay={200}
+                        video={barrelrollDemo}
                     />
                     <AnimatedSection 
                         title="SoundScape: VR musical playground"
-                        delay={200}
-                        
+                        delay={700}
+                        video={soundscapeDemo}
                     />
                 </div>
             </div>
